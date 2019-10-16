2f86f0c29b66c23af8e59b2b40334bacec476bf8
Bihelca Quinonez <bquinonez@my.uri.edu>
2019-10-16 00:15:17 +0000

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,20 @@
+class Artist 
+  attr_accessor :name 
+  
+  @@songs = 0
+  
+  def initialize(name)
+    @name = name
+  end
+  
+  def add_song(song) 
+    song.artist = self
+  end
+    
+  def add_song_by_name(name, genre)
+    song = Song.new(name, genre)
+    add_song(song)
+  end
+  
+  
+end
