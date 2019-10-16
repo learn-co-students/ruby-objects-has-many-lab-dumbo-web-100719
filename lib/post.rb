918dfa7a026572f5abbeb244113b09a4f54d55ac
Bihelca Quinonez <bquinonez@my.uri.edu>
2019-10-16 01:17:32 +0000

    Done.

--- lib/post.rb
+++ lib/post.rb
@@ -1,11 +1,19 @@
 class Post 
-  attr_accessor :title 
+  attr_accessor :title, :author
   
   @@all = []
   
   def initialize(title)
     @title = title
-    @@all << title 
+    @@all << self 
   end
   
-en
+  def self.all 
+    @@all
+  end
+  
+  def author_name
+    author.name if author
+  end
+  
+end
